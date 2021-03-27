cd3597ad357c6a90d6c6e3d37d458204cdbf00ab
AlexSytsenko <sytsenko@gmail.com>
2021-03-27 21:42:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  render() {
+    return;
+    <></>;
+  }
+}
+
+export default App;
